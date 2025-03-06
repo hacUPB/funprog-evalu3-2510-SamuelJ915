e36370b1b352e5f97198238db04619a2f417a7fd
SamuelJ915 <samu.jimenez01@gmail.com>
2025-03-06 14:12:17 +0000

    Se logra corregir un error

--- Actividad1/hola.s
+++ Actividad1/hola.s
@@ -1,5 +1,29 @@
 	.file	"hola.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"\302\241Hola Henry!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
